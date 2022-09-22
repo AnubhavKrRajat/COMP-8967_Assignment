8b17269436e8988d52a03cf2ae2d2dcd1e08dc44
GabaniYash <99998506+GabaniYash@users.noreply.github.com>
2022-09-22 18:26:19 -0400

    Added Profile page route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import Home from "./components/Home";
 import React from "react";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Profile from "./components/Profile";
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </div>
   );
